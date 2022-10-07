e87eed24de72c612f286c4bdc4e21888ccfddd2c
shankarsaravanan357 <shankarsaravanan007@gmail.com>
2022-10-07 10:19:22 +0530

    post details page created and fixed the link to that page

--- blogapp/blog/urls.py
+++ blogapp/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, about
+from .views import home, about, PostListView, PostDetailView
 
 
 urlpatterns = [
-    path('', home, name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('about/', about, name="blog-about")
 ]
